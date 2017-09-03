31e0eedb291ef95992e6dd3789f0c4483b9185ea
linhy112346 <394743845@qq.com>
2017-09-03 15:46:35 +0800

    第一次提交

---
+++ app/src/main/java/com/example/linhy/handle/DateActivity.java
@@ -0,0 +1,18 @@
+package com.example.linhy.handle;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by linhy on 17-9-3.
+ */
+
+public class DateActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
